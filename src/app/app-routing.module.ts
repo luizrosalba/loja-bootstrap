fbbdda53969eb4e399ec0d26c780b44a93998f11
Luiz <luizrosalba@gmail.com>
2020-11-05 18:55:36 -0300

    footer header e body funcionam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {HeaderComponent} from './header/header.component'
+import { TestesComponent } from './testes/testes.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: 'header', component: HeaderComponent, data: { title: 'HeaderComponent' } },
+  
+  { path: '', component: TestesComponent },
+  
+  
+    
 ];
 
 @NgModule({
